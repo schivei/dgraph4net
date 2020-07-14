c12e1750a30d5325eaafb18136e1940acc35a305
Elton Schivei Costa <costa@elton.schivei.nom.br>
2020-07-14 00:08:17 -0300

    Change build type

--- pack.sh
+++ pack.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-dotnet build -c Release
-dotnet pack -c Release -o ./build-packages -p:IncludeSymbols=false
+dotnet restore src/Dgraph4Net.Core/Dgraph4Net.Core.csproj
+dotnet restore src/Dgraph4Net/Dgraph4Net.csproj
+dotnet restore src/Dgraph4Net.Identity.Core/Dgraph4Net.Identity.Core.csproj
+dotnet restore src/Dgraph4Net.Identity/Dgraph4Net.Identity.csproj
+
+dotnet build -c Release --no-restore src/Dgraph4Net.Core/Dgraph4Net.Core.csproj
+dotnet build -c Release --no-restore src/Dgraph4Net/Dgraph4Net.csproj
+dotnet build -c Release --no-restore src/Dgraph4Net.Identity.Core/Dgraph4Net.Identity.Core.csproj
+dotnet build -c Release --no-restore src/Dgraph4Net.Identity/Dgraph4Net.Identity.csproj
+
+dotnet pack -c Release --no-build -o ./build-packages -p:IncludeSymbols=false src/Dgraph4Net.Core/Dgraph4Net.Core.csproj
+dotnet pack -c Release --no-build -o ./build-packages -p:IncludeSymbols=false src/Dgraph4Net/Dgraph4Net.csproj
+dotnet pack -c Release --no-build -o ./build-packages -p:IncludeSymbols=false src/Dgraph4Net.Identity.Core/Dgraph4Net.Identity.Core.csproj
+dotnet pack -c Release --no-build -o ./build-packages -p:IncludeSymbols=false src/Dgraph4Net.Identity/Dgraph4Net.Identity.csproj
 dotnet nuget push ./build-packages/*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
