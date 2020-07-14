c159828f3042c7be0b7ab3e9ea2aeecfd31703cd
Elton Schivei Costa <costa@elton.schivei.nom.br>
2020-07-14 00:23:20 -0300

    Step-by-step publish package

--- pack.sh
+++ pack.sh
@@ -13,4 +13,8 @@ dotnet pack -c Release --no-build -o ./build-packages -p:IncludeSymbols=false sr
 dotnet pack -c Release --no-build -o ./build-packages -p:IncludeSymbols=false src/Dgraph4Net/Dgraph4Net.csproj
 dotnet pack -c Release --no-build -o ./build-packages -p:IncludeSymbols=false src/Dgraph4Net.Identity.Core/Dgraph4Net.Identity.Core.csproj
 dotnet pack -c Release --no-build -o ./build-packages -p:IncludeSymbols=false src/Dgraph4Net.Identity/Dgraph4Net.Identity.csproj
-dotnet nuget push ./build-packages/*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+
+dotnet nuget push ./build-packages/Dgraph4Net.Core.1.0.0.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push ./build-packages/Dgraph4Net.1.0.0.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push ./build-packages/Dgraph4Net.Identity.Core.1.0.0.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push ./build-packages/Dgraph4Net.Identity.1.0.0.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
