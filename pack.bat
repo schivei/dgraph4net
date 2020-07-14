76250e35703da818506843fbbad2159af1e240e6
Elton Schivei Costa <costa@elton.schivei.nom.br>
2020-07-13 23:30:46 -0300

    Change build packages path

--- pack.bat
+++ pack.bat
@@ -1,4 +1,3 @@
 dotnet build -c Release
-md ..\packages 2> nul
-dotnet pack -c Release -o ..\packages -p:IncludeSymbols=false
-dotnet nuget push ..\packages\*.nupkg -k %1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet pack -c Release -o .\build-packages -p:IncludeSymbols=false
+dotnet nuget push .\build-packages\*.nupkg -k %1 -s https://api.nuget.org/v3/index.json --skip-duplicate
--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dotnet build -c Release
-dotnet pack -c Release -o packages -p:IncludeSymbols=false
-dotnet nuget push packages/*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet pack -c Release -o ./build-packages -p:IncludeSymbols=false
+dotnet nuget push ./build-packages/*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
