bb372568f94e5a9c4e9522b942598c356cbec329
Elton Schivei Costa <costa@elton.schivei.nom.br>
2020-07-13 23:26:27 -0300

    Change Pack

--- pack.sh
+++ pack.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 dotnet build -c Release
-mkdir -p ../packages &2> /dev/null
-dotnet pack -c Release -o ../packages -p:IncludeSymbols=false
-dotnet nuget push ../packages/*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet pack -c Release -o packages -p:IncludeSymbols=false
+dotnet nuget push packages/*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
