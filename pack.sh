31fe5fdd8ceeb2527568f20d8d9672aa0d96dd54
Elton Schivei Costa <costa@elton.schivei.nom.br>
2023-05-30 19:37:07 -0300

    Naming changes

--- pack.sh
+++ pack.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 dotnet build -c Release ./src/Dgraph4Net.Core/Dgraph4Net.Core.csproj
-dotnet build -c Release ./src/DGraph4Net/Dgraph4Net.csproj
+dotnet build -c Release ./src/Dgraph4Net/Dgraph4Net.csproj
 dotnet build -c Release ./tools/Dgraph4Net.Tools/Dgraph4Net.Tools.csproj
 
 dotnet pack -c Release -o ./build-packages -p:IncludeSymbols=false ./src/Dgraph4Net.Core/Dgraph4Net.Core.csproj
-dotnet pack -c Release -o ./build-packages -p:IncludeSymbols=false ./src/DGraph4Net/Dgraph4Net.csproj
+dotnet pack -c Release -o ./build-packages -p:IncludeSymbols=false ./src/Dgraph4Net/Dgraph4Net.csproj
 dotnet pack -c Release -o ./build-packages -p:IncludeSymbols=false ./tools/Dgraph4Net.Tools/Dgraph4Net.Tools.csproj
